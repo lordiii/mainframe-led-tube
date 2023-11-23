a8f24f491d687208c5ca4c8e5f29e15c99805c5b
Lordiii <lordiii@lordiii.de>
2023-11-23 19:35:48 +0100

    Firmware: Use QNEthernet

--- firmware/include/main.h
+++ firmware/include/main.h
@@ -1,26 +1,12 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <INA226.h>
-#include <NativeEthernet.h>
-#include <NativeEthernetUdp.h>
+#include <QNEthernet.h>
 #include <OctoWS2811.h>
 
-#pragma region Network
+using namespace qindesign::network;
 
-bool initializeDHCP();
-void checkDHCPStatus();
-
-#pragma endregion
-
-#pragma region Sensors
-
-void initializeSensors();
 bool initializeCurrentSensor(INA226 *sensor);
-
-#pragma endregion
-
-#pragma region LED Control
-
 void testLEDColors();
 
-#pragma endregion
+void colorWipe(int color, int wait);
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,109 +1,18 @@
 #include "main.h"
 #include "globals.h"
 
-byte mac[] = MAC_ADDRESS;
+// Setup LEDs
+const byte pinList[LED_STRIP_AMOUNT] = LED_PINS;
+const int numLeds = LED_PER_STRIP * LED_STRIP_AMOUNT;
+const int numberOfChannels = numLeds * 3; // Total number of channels you want to receive (1 led = 3 channels)
 
-void setup()
-{
-    Wire.begin();
-    Serial.begin(115200);
+DMAMEM int displayMemory[LED_PER_STRIP * 6];
+int drawingMemory[LED_PER_STRIP * 6];
 
-    Serial.println("Fetching IP Address via DHCP");
-    bool hasIP = initializeDHCP();
-    if (!hasIP)
-    {
-        while (!hasIP)
-        {
-            delay(1000);
-            hasIP = initializeDHCP();
-            if (hasIP)
-            {
-                Serial.println("IP Address assigned....continuing!");
-            }
-        }
-    }
+const int config = LED_CONFIGURATION;
+OctoWS2811 leds(LED_PER_STRIP, displayMemory, drawingMemory, config, LED_STRIP_AMOUNT, pinList);
 
-    testLEDColors();
-    initializeSensors();
-}
-
-long lastMillis = 0;
-void loop()
-{
-    // Perform other tasks every 2.5 second
-    if ((millis() - lastMillis) < 2500)
-    {
-
-    }
-    else
-    {
-        lastMillis = millis();
-        checkDHCPStatus();
-    }
-}
-
-#pragma region Network
-
-void checkDHCPStatus()
-{
-    switch (Ethernet.maintain())
-    {
-    case 1:
-        // renewed fail
-        Serial.println("Error: renewed fail");
-        break;
-
-    case 2:
-        // renewed success
-        Serial.println("Renewed success");
-        // print your local IP address:
-        Serial.print("My IP address: ");
-        Serial.println(Ethernet.localIP());
-        break;
-
-    case 3:
-        // rebind fail
-        Serial.println("Error: rebind fail");
-        break;
-
-    case 4:
-        // rebind success
-        Serial.println("Rebind success");
-        // print your local IP address:
-        Serial.print("My IP address: ");
-        Serial.println(Ethernet.localIP());
-        break;
-
-    default:
-        // nothing happened
-        break;
-    }
-}
-
-bool initializeDHCP()
-{
-    if (Ethernet.begin(mac) == 0)
-    {
-        if (Ethernet.linkStatus() == LinkOFF)
-        {
-            Serial.println("Ethernet cable is not connected. Retrying every 1 second...");
-            return false;
-        }
-
-        Serial.println("Unknown Ethernet error... Sorry, can't run without hardware. :(");
-        while (true)
-        {
-            delay(1);
-        }
-    }
-
-    return true;
-}
-
-#pragma endregion
-
-#pragma region Sensors
-
+// Create Current Sensor Objects
 const uint8_t currentSensor1Address = CURRENT_SENSOR_1_ADDRESS;
 INA226 *currentSensor1 = new INA226(currentSensor1Address);
 
@@ -113,12 +22,65 @@ INA226 *currentSensor2 = new INA226(currentSensor2Address);
 const uint8_t currentSensor3Address = CURRENT_SENSOR_3_ADDRESS;
 INA226 *currentSensor3 = new INA226(currentSensor3Address);
 
-void initializeSensors()
+void setup()
 {
+
+    Wire.begin();
+    Serial.begin(115200);
+
+    Ethernet.begin();
+    while (!Ethernet.waitForLocalIP(15000))
+    {
+        Serial.println("Waiting for IP Address");
+    }
+
+    Serial.println("Testing LEDs");
+    leds.begin();
+    leds.show();
+    testLEDColors();
+
+    Serial.println("Initializing Current Sensors");
     initializeCurrentSensor(currentSensor1);
     initializeCurrentSensor(currentSensor2);
     initializeCurrentSensor(currentSensor3);
 }
+
+
+#define RED    0xFF0000
+#define GREEN  0x00FF00
+#define BLUE   0x0000FF
+
+// Less intense...
+/*
+#define RED    0x160000
+#define GREEN  0x001600
+#define BLUE   0x000016
+#define YELLOW 0x101400
+#define PINK   0x120009
+#define ORANGE 0x100400
+#define WHITE  0x101010
+*/
+
+void loop() {
+  colorWipe(0, 25);
+  colorWipe(RED, 25);
+  colorWipe(0, 25);
+  colorWipe(RED, 25);
+  colorWipe(0, 25);
+  colorWipe(BLUE, 25);
+  colorWipe(0, 25);
+  colorWipe(BLUE, 25);
+}
+
+void colorWipe(int color, int wait)
+{
+  for (int i=0; i < leds.numPixels(); i++) {
+    leds.setPixel(i, color);
+  }
+  leds.show();
+  delay(wait);
+}
+
 
 bool initializeCurrentSensor(INA226 *sensor)
 {
@@ -136,58 +98,38 @@ bool initializeCurrentSensor(INA226 *sensor)
     }
 }
 
-#pragma endregion
-
-#pragma region LED Control
-
-/**
- *
- * Heavily Based on the ArtnetOctoWS2811 Example of https://github.com/natcl/Artnet/
- *
- */
-
 // OctoWS2811 settings
-const byte pinList[LED_STRIP_AMOUNT] = LED_PINS;
-
-const int numLeds = LED_PER_STRIP * LED_STRIP_AMOUNT;
-
-const int numberOfChannels = numLeds * 3; // Total number of channels you want to receive (1 led = 3 channels)
-
-DMAMEM int displayMemory[LED_PER_STRIP * 6];
-int drawingMemory[LED_PER_STRIP * 6];
-
-const int config = LED_CONFIGURATION;
-OctoWS2811 leds(LED_PER_STRIP, displayMemory, drawingMemory, config, LED_STRIP_AMOUNT, pinList);
 
 // Check if we got all universes
 void testLEDColors()
 {
-    // Try Red LEDs
     for (int i = 0; i < numLeds; i++)
     {
         leds.setPixel(i, 127, 0, 0);
     }
+    Serial.println("Testing Red LEDs");
     leds.show();
 
     delay(500);
 
-    // Try Green LEDs
     for (int i = 0; i < numLeds; i++)
     {
         leds.setPixel(i, 0, 127, 0);
     }
+    Serial.println("Testing Green LEDs");
     leds.show();
 
     delay(500);
 
-    // Try Blue LEDs
     for (int i = 0; i < numLeds; i++)
     {
         leds.setPixel(i, 0, 0, 127);
     }
+    Serial.println("Testing Blue LEDs");
     leds.show();
 
     delay(500);
+
 
     for (int i = 0; i < numLeds; i++)
     {
@@ -195,5 +137,3 @@ void testLEDColors()
     }
     leds.show();
 }
-
-#pragma endregions
