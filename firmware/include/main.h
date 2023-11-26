0d8b1ffccdfd8a4746373520268ff0d6fc9b8abc
Lordiii <lordiii@lordiii.de>
2023-11-26 16:32:47 +0100

    Firmware: Read Temperatures

--- firmware/include/main.h
+++ firmware/include/main.h
@@ -3,10 +3,11 @@
 #include <INA226.h>
 #include <QNEthernet.h>
 #include <OctoWS2811.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 using namespace qindesign::network;
 
 bool initializeCurrentSensor(INA226 *sensor);
 void testLEDColors();
-
 void colorWipe(int color, int wait);
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -13,31 +13,48 @@ const int config = LED_CONFIGURATION;
 OctoWS2811 leds(LED_PER_STRIP, displayMemory, drawingMemory, config, LED_STRIP_AMOUNT, pinList);
 
 // Create Current Sensor Objects
-const uint8_t currentSensor1Address = CURRENT_SENSOR_1_ADDRESS;
+const unsigned char currentSensor1Address = CURRENT_SENSOR_1_ADDRESS;
 INA226 *currentSensor1 = new INA226(currentSensor1Address);
 
-const uint8_t currentSensor2Address = CURRENT_SENSOR_2_ADDRESS;
+const unsigned char currentSensor2Address = CURRENT_SENSOR_2_ADDRESS;
 INA226 *currentSensor2 = new INA226(currentSensor2Address);
 
-const uint8_t currentSensor3Address = CURRENT_SENSOR_3_ADDRESS;
+const unsigned char currentSensor3Address = CURRENT_SENSOR_3_ADDRESS;
 INA226 *currentSensor3 = new INA226(currentSensor3Address);
+
+OneWire oneWire(16);
+DallasTemperature sensors(&oneWire);
+const unsigned char tempProbeUpper[8] = TEMPERATURE_SENSOR_TOP_ADDRESS;
+const unsigned char tempProbeMiddle[8] = TEMPERATURE_SENSOR_MIDDLE_ADDRESS;
+const unsigned char tempProbeLower[8] = TEMPERATURE_SENSOR_BOTTOM_ADDRESS;
 
 void setup()
 {
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
 
     Wire.begin();
     Serial.begin(115200);
 
     Ethernet.begin();
-    while (!Ethernet.waitForLocalIP(15000))
-    {
-        Serial.println("Waiting for IP Address");
-    }
+    //while (!Ethernet.waitForLocalIP(15000))
+    //{
+    //    Serial.println("Waiting for IP Address");
+    //}
 
-    Serial.println("Testing LEDs");
+    Serial.println("Current IP: ");
+    Serial.print(Ethernet.localIP()[0]);
+    Serial.print(".");
+    Serial.print(Ethernet.localIP()[1]);
+    Serial.print(".");
+    Serial.print(Ethernet.localIP()[2]);
+    Serial.print(".");
+    Serial.print(Ethernet.localIP()[3]);
+
     leds.begin();
     leds.show();
     testLEDColors();
+
 
     Serial.println("Initializing Current Sensors");
     initializeCurrentSensor(currentSensor1);
@@ -45,42 +62,47 @@ void setup()
     initializeCurrentSensor(currentSensor3);
 }
 
+#define RED 0xFF0000
+#define GREEN 0x00FF00
+#define BLUE 0x0000FF
 
-#define RED    0xFF0000
-#define GREEN  0x00FF00
-#define BLUE   0x0000FF
+long last = 0;
+void loop()
+{
+    testLEDColors();
 
-// Less intense...
-/*
-#define RED    0x160000
-#define GREEN  0x001600
-#define BLUE   0x000016
-#define YELLOW 0x101400
-#define PINK   0x120009
-#define ORANGE 0x100400
-#define WHITE  0x101010
-*/
+    if (millis() - last > 2500)
+    {
+        sensors.requestTemperatures();
+        float tempCTop = sensors.getTempC(tempProbeUpper);
+        float tempCMiddle = sensors.getTempC(tempProbeMiddle);
+        float tempCBottom = sensors.getTempC(tempProbeLower);
 
-void loop() {
-  colorWipe(0, 25);
-  colorWipe(RED, 25);
-  colorWipe(0, 25);
-  colorWipe(RED, 25);
-  colorWipe(0, 25);
-  colorWipe(BLUE, 25);
-  colorWipe(0, 25);
-  colorWipe(BLUE, 25);
+        Serial.print("Temperatures: ");
+        Serial.print(tempCTop);
+        Serial.print(" \xC2\xB0");
+        Serial.print("C  Top |  ");
+        Serial.print(tempCMiddle);
+        Serial.print(" \xC2\xB0");
+        Serial.print("C  Middle |  ");
+        Serial.print(tempCBottom);
+        Serial.print(" \xC2\xB0");
+        Serial.print("C Bottom");
+        Serial.println();
+
+        last = millis();
+    }
 }
 
 void colorWipe(int color, int wait)
 {
-  for (int i=0; i < leds.numPixels(); i++) {
-    leds.setPixel(i, color);
-  }
-  leds.show();
-  delay(wait);
+    for (int i = 0; i < leds.numPixels(); i++)
+    {
+        leds.setPixel(i, color);
+    }
+    leds.show();
+    delay(wait);
 }
-
 
 bool initializeCurrentSensor(INA226 *sensor)
 {
@@ -103,6 +125,8 @@ bool initializeCurrentSensor(INA226 *sensor)
 // Check if we got all universes
 void testLEDColors()
 {
+    Serial.println("Testing LEDs");
+
     for (int i = 0; i < numLeds; i++)
     {
         leds.setPixel(i, 127, 0, 0);
@@ -129,7 +153,6 @@ void testLEDColors()
     leds.show();
 
     delay(500);
-
 
     for (int i = 0; i < numLeds; i++)
     {
