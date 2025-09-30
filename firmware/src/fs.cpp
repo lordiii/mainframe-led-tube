98db8d8641d2bbafbdee6fad46064901aa23517a
Lordiii <lordiii@lordiii.de>
2025-09-30 18:32:29 +0200

    feat: add sd card support

---
+++ firmware/src/fs.cpp
@@ -0,0 +1,74 @@
+﻿#include "fs.h"
+
+#include <Arduino.h>
+#include <SdFat.h>
+
+SdFat32 sd;
+File32 file;
+File32 dir;
+
+#define SD_CONFIG SdioConfig(FIFO_SDIO)
+#define SPI_CONFIG SdSpiConfig(TeensySdioCard, DEDICATED_SPI, SPI_FULL_SPEED, &SPI2)
+
+DirectoryListing directoryListing;
+
+void SD_init() {
+    if (!sd.begin(SD_CONFIG)) {
+        sd.initErrorHalt();
+    }
+}
+
+void SD_format() {
+    sd.format(&Serial);
+}
+
+void SD_assertDirectory(const char *directory) {
+    if (!dir.exists(directory)) {
+        sd.mkdir(directory);
+    }
+}
+
+int SD_loadFile(const char *filepath, char* buf, size_t size) {
+    if (!file.open(filepath)) {
+        // TODO: LOGGING
+        return false;
+    }
+
+    return file.read(buf, size);
+}
+
+int SD_loadFile(const char *directory, const char *filename, char* buf, size_t size) {
+    size_t len = strlen(directory) + strlen(filename) + 1;
+    char str[len];
+    str[0] = '\0';
+
+    strcat(str, directory);
+    strcat(str, "/");
+    strcat(str, filename);
+
+    return SD_loadFile(str, buf, size);
+}
+
+DirectoryListing *SD_listFiles(const char *directory, const char *suffix) {
+    if (!dir.open(directory)) {
+        // TODO: LOGGING
+        return nullptr;
+    }
+
+    directoryListing.fileCount = 0;
+    for (int i = 0; i < FILE_LIST_BUFFER_SIZE; i++) {
+        directoryListing.fileNames[i][0] = '\0';
+    }
+
+    while (file.openNext(&dir)) {
+        char *filename = file.name();
+        if (file.isHidden() || file.isDir() || strlen(filename) > FILE_LIST_NAME_LIMIT || (suffix != nullptr && strstr(filename, suffix) == nullptr)) continue;
+
+        strcpy(directoryListing.fileNames[directoryListing.fileCount], filename);
+        file.close();
+    }
+
+    dir.close();
+
+    return &directoryListing;
+}
---
+++ firmware/src/fs.h
@@ -0,0 +1,22 @@
+﻿#ifndef SD_H
+#define SD_H
+
+#define FILE_LIST_BUFFER_SIZE 32
+#define FILE_LIST_NAME_LIMIT 64
+
+struct DirectoryListing {
+  int fileCount;
+  char fileNames[FILE_LIST_BUFFER_SIZE][FILE_LIST_NAME_LIMIT];
+};
+
+int SD_loadFile(const char *filepath, char* buf, size_t size);
+
+int SD_loadFile(const char *directory, const char *filename, char* buf, size_t size);
+
+void SD_format();
+
+void SD_assertDirectory(const char *directory);
+
+DirectoryListing *SD_listFiles(const char *directory, const char *suffix = nullptr);
+
+#endif
